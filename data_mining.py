1c6d09eb33cfe8c82ec0d2e071f57a527075f36f
becerraj <becerra.jc99@gmail.com>
2023-09-08 22:20:07 -0300

    Normalizing and Standarizing in R

--- data_mining.py
+++ data_mining.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import openpyxl
+from sklearn.preprocessing import MinMaxScaler
 
 # #CARGO DATA----
 # # Cargo el data_set
@@ -9,155 +10,173 @@ import openpyxl
 data_set = pd.read_excel("/home/juan/Github/Data_Science_2023/CartagenaCohortStudy_DATA.xlsx")
 
 # #EXTRACT DATA----
-tratamiento_hta = data_set.iloc[:, 11:54]
+tratamiento_hta = data_set.loc[:, ['tto_hta_p___1',
+                                   'tto_hta_p___2', 'tto_hta_p___3', 'tto_hta_p___4', 'tto_hta_p___5',
+                                   'tto_hta_p___6', 'tto_hta_p___7', 'tto_hta_p___8', 'tto_hta_p___9',
+                                   'cual_araii___1', 'cual_araii___2', 'cual_araii___3', 'cual_araii___4',
+                                   'cual_ca___1', 'cual_ca___2', 'cual_ca___3', 'cual_ca___4',
+                                   'cual_ca___5', 'cual_ca___6', 'cual_ca___7', 'cual_ca___8',
+                                   'cual_ca___9', 'cual_bb___1', 'cual_bb___2', 'cual_bb___3',
+                                   'cual_bb___4', 'cual_bb___5', 'cual_bb___6', 'cual_bb___7',
+                                   'cual_bb___8', 'cual_bb___9', 'cual_bb___10', 'cual_ieca___1',
+                                   'cual_ieca___2', 'cual_ieca___3', 'cual_ieca___4', 'cual_ieca___5',
+                                   'cual_diu___1', 'cual_diu___2', 'cual_diu___3', 'cual_diu___4',
+                                   'cual_digi___1', 'cual_digi___2', 'tto_hta_p_otro']]
+
+
+# Busca si en la fila se encuentra un 1 en alguna celda, y devuelve el valor 1.99881. Si no hay ningun 1, devuelve 1.93303
+def buscarUno(fila):
+    if (fila == 1).any():
+        return 1.99881
+    else:
+        return 1.93303
+
 
 hay_tratamiento = []
-hay_tratamiento = tratamiento_hta.apply(buscarUno, axis = 1)
+# Apply genera una iteracion y va guardando interiormente los resultados devueltos.
+hay_tratamiento = tratamiento_hta.apply(buscarUno, axis=1).tolist()
 
-def buscarUno(tratamiento_hta):
-    if (tratamiento_hta[:] == 1).any():
-        hay_tratamiento.append(1.99881)
-    else:
-        hay_tratamiento.append(1.93303)
-    return(hay_tratamiento)
+# Elimino las columnas que son ttos. Tambien las de ID
+# indices_a_eliminar = list(range(11, 54+1)) + list(range(70, 78+1))
+# Posibilidades de eliminar columnas:
+# data_set = data_set.drop(data_set.columns[indices_a_eliminar], axis = 1)
+data_set = data_set.drop(['study_id', 'tto_hta_p___1', 'dis_p_act',
+                          'tto_hta_p___2', 'tto_hta_p___3', 'tto_hta_p___4', 'tto_hta_p___5',
+                          'tto_hta_p___6', 'tto_hta_p___7', 'tto_hta_p___8', 'tto_hta_p___9',
+                          'cual_araii___1', 'cual_araii___2', 'cual_araii___3', 'cual_araii___4',
+                          'cual_ca___1', 'cual_ca___2', 'cual_ca___3', 'cual_ca___4',
+                          'cual_ca___5', 'cual_ca___6', 'cual_ca___7', 'cual_ca___8',
+                          'cual_ca___9', 'cual_bb___1', 'cual_bb___2', 'cual_bb___3',
+                          'cual_bb___4', 'cual_bb___5', 'cual_bb___6', 'cual_bb___7',
+                          'cual_bb___8', 'cual_bb___9', 'cual_bb___10', 'cual_ieca___1',
+                          'cual_ieca___2', 'cual_ieca___3', 'cual_ieca___4', 'cual_ieca___5',
+                          'cual_diu___1', 'cual_diu___2', 'cual_diu___3', 'cual_diu___4',
+                          'cual_digi___1', 'cual_digi___2', 'tto_hta_p_otro', 'cvf_prev',
+                          'vef1_prev', 'vef1_cvf_prev', 'pef_prev', 'vef1_pre', 'cvf_pre',
+                          'vef1_cvf_pre', 'pef_pre', 'vef1_pre_por', 'a_vertebral',
+                          'insuficiencia_vb'], axis=1)
 
-
-hay_tratamiento = []
-# Tener en cuenta que la dimension real de los datos es de len(), pero su indexacion comienza en 0.
-# Entonces los range si bien no toman el ultimo valor, al quedarse con el anterior no afectan al bucle.
-for i in range(0, len(data_set)):
-    if (tratamiento_HTA.iloc[i] == 1).any():
-        hay_tratamiento.append(1.99881)
-    else:
-        hay_tratamiento.append(0)
-
-# Elimino las columnas que son ttos
-indices_a_eliminar = list(range(11, 54+1)) + list(range(70, 78+1))
-data_set = data_set.drop(data_set.columns[indices_a_eliminar], axis = 1)
+# Agrego la columna que dice si hay tratamiento
 data_set['hay_tto'] = hay_tratamiento
 
 # Que tipos de dato tengo?
+description = data_set.describe()
 data_set.dtypes
-# vef1_cvf_pre_por y intima_m_c_d deberian ser numericos
-data_set['intima_m_c_d'] = data_set['intima_m_c_d'].str.replace(",", ".").str.replace(r'[^0-9]', "")
 
-data_set['intima_m_c_d'] = data_set['intima_m_c_d'].astype(float)
+# Sigo achicando el numero de columnas: IMC = Peso + talla. ICC = PC + PC
+data_set = data_set.drop(['peso', 'talla', 'per_cin', 'per_cad', 'i_m_carotidea',
+                          'localidad'], axis=1)
 
-data_set['vef1_cvf_pre_por'] = (data_set['vef1_cvf_pre_por'].astype(float)
-
-# Paso los valores de gender a factores:
-mapeo = {0: 'Male', 1: 'Female'}
-data_set['gender'] = data_set['gender'].map(mapeo)
-
-
+# Ahora ya tengo todas las columnas que me interesan. Voy a sacar las filas con NAN
 # # Cuantos NA tengo?
 data_set.isna().sum().sum()
-# Tengo 590 NA
+# Tengo 465 NA
 data_set.isna().any(axis=1).sum()
 # Hay NA en 111 filas. Muchos pacientes tienen +1 datos con NA
 # Que porcentaje de NA tengo en cada columna?
 data_set.isna().sum() / data_set.shape[0] * 100
 
+# Si bien esto deberiamos hacerlo despues de sacar todas las variables que no nos interesan, para simplificar
+# el analisis, lo hacemos ahora.
 data_set = data_set.dropna()
-# Teniendo en cuenta que me voy a quedar sin las columnas de parametros sericos (analisis de sangre)
-# primero voy a analizar esos datos, eliminar los datos que sean outliers, y luego eliminar esas columnas
-# Despues de esto analizo los datos NA faltantes para ver como tratarlos.
-sns.boxplot(x = 'gender', y = 'dis_p', data = data_set)
-sns.boxplot(x = 'gender', y = 'dis_p', data = data_set)
 
-sns.violinplot(x = 'gender', y = 'dis_p', data = data_set)
-# Elimino las filas que tienen NA. A otra cosa jajaja
-# data_set <- na.omit(data_set)
+# vef1_cvf_pre_por y intima_m_c_d deberian ser numericos
+# Primero lo paso a un tipo de dato str para que funcione str.replace. Antes era un object
+intima_to_float = data_set['intima_m_c_d'].astype(str).str.replace(",", ".").str.replace("|", "")
+data_set['intima_m_c_d'] = intima_to_float.astype(float)
+data_set['vef1_cvf_pre_por'] = data_set['vef1_cvf_pre_por'].astype(float)
+data_set[['gender', 'placas_atero']] = data_set[['gender', 'placas_atero']].astype(int)
 
 
+# Algunas variables tienen 2=Si, 1=No. Lo cambio para normalizar con el resto de variables y binarizar luego
+def two_to_one(fila):
+    if fila == 2:
+        return 1
+    else:
+        return 0
 
-# hay_tto <- c()
-# for (i in (1:nrow(cols_tto_hta))) {
-#   if (1 %in% cols_tto_hta[i, ]) {
-#     hay_tto <- c(hay_tto, 1.99881)
-#   } else {
-#     hay_tto <- c(hay_tto, 1.93303)
-#   }
-# }
 
-# # Elimino las columnas que son ttos
-# data_set <- data_set[, -c(11:54, 70:78)]
-# data_set <- as.data.frame(data_set)
-# data_set <- cbind(data_set, hay_tto)
-#
-# # Que tipos de dato tengo?
-# for (i in (1:ncol(data_set))) {
-#   print(sprintf("%s: %s", colnames(data_set[i]), class(data_set[, i])))
-# }
-#
-# # Cuantos NA tengo?
-# colSums(is.na(data_set))
-#
-# # Que porcentaje de cada columna es eso?
-# # print(sprintf("%s %s",(na_counts/nrow(data_set))*100, "%"))
-#
-# # Elimino las columnas con muy alta proporcion de NA
-# # data_set <- data_set[, -c(34, 37)]
-#
-# # Para hacer boxplot + violin plot de los datos voy a sacar a las variables que no sean numericas
-# # study_id, tipo_id, gender, num_indi_vivienda_v2, ingresos,
-# data_clean <- data_set
-# data_set$study_id <- NULL
-# data_set$tipo_id <- NULL
-#
-#
-# for (i in (1:ncol(data_set))) {
-#   Value <- data_set[, i]
-#   Group <- rep(colnames(data_set)[i], nrow(data_set))
-#   grafico <- ggplot(data_set, aes(x = Group, y = Value)) +
-#     geom_violin() +
-#     geom_boxplot(width = 0.2) +
-#     labs(title = sprintf("%s", Group[1]))
-#
-#   print(grafico)
-# }
-#
-# index_F<- grep(0, data_set$gender)
-# index_M <- grep(1, data_set$gender)
-# data_set$gender[index_F] <- 1
-# data_set$gender[index_M] <- 0
-#
-# index_no <- grep(1, data_set$expo_tab_otro)
-# index_si <- grep(2, data_set$expo_tab_otro)
-# data_set$expo_tab_otro[index_no] <- 0
-# data_set$expo_tab_otro[index_si] <- 1
-#
-# index_no <- grep(1, data_set$dis_p)
-# index_si <- grep(2, data_set$dis_p)
-# data_set$dis_p[index_no] <- 0
-# data_set$dis_p[index_si] <- 1
-#
-# index_no <- grep(1, data_set$hta_p)
-# index_si <- grep(2, data_set$hta_p)
-# data_set$hta_p[index_no] <- 0
-# data_set$hta_p[index_si] <- 1
-#
-# index_no <- grep(1, data_set$ecv_p)
-# index_si <- grep(2, data_set$ecv_p)
-# data_set$ecv_p[index_no] <- 0
-# data_set$ecv_p[index_si] <- 1
-#
-# index_no <- grep(1, data_set$dm2_p)
-# index_si <- grep(2, data_set$dm2_p)
-# data_set$dm2_p[index_no] <- 0
-# data_set$dm2_p[index_si] <- 1
-#
-# data_set$intima_m_c_d <- as.numeric(data_set$intima_m_c_d)
-# data_set$vef1_cvf_pre_por <- as.numeric(data_set$vef1_cvf_pre_por)
-#
+# La variable .apply() sabe cuantos ejes tiene el df que le estamos dando.
+# Si tiene solo 1, no hay que ponerle axis. Sino se rompe.
+data_set['expo_tab_otro'] = data_set['expo_tab_otro'].apply(two_to_one)
+data_set['ant_p_enf'] = data_set['ant_p_enf'].apply(two_to_one)
+data_set['dis_p'] = data_set['dis_p'].apply(two_to_one)
+data_set['hta_p'] = data_set['hta_p'].apply(two_to_one)
+data_set['ecv_p'] = data_set['ecv_p'].apply(two_to_one)
+data_set['dm2_p'] = data_set['dm2_p'].apply(two_to_one)
+
+# Hago y analizo un boxplot y violin plot de cada variable que no es binaria para ver los outliers
+for i in range(0, len(data_set.columns)):
+    if data_set.iloc[:, i].dtypes == float:
+        fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(9, 4))
+        axs[0].violinplot(data_set.iloc[:, i],
+                          showmeans=False,
+                          showmedians=True)
+        axs[0].set_title('Violin plot')
+
+        # plot box plot
+        axs[1].boxplot(data_set.iloc[:, i])
+        axs[1].set_title('Box plot')
+
+        # adding horizontal grid lines
+        for ax in axs:
+            ax.yaxis.grid(True)
+            ax.set_xlabel(data_set.columns[i])
+            ax.set_ylabel('Values')
+
+        plt.show()
+
+# Luego del analisis del boxplot elimino los outliers
+# Teniendo en cuenta la cantidad de datos que tenemos y los outliers vistos,
+# Voy a sacar los outliers segun el test de Tukey. Tomando rango intercuartilico:
+description = data_set.describe()
+indices_a_eliminar = []
+for i in range(0, len(data_set.columns) - 1):
+    if data_set.iloc[:, i].dtypes == float:
+        limite_superior = ((description.iloc[:, i]['75%'] - description.iloc[:, i]['25%']) * 2) + \
+                          description.iloc[:, i]['75%']
+        limite_inferior = description.iloc[:, i]['25%'] - (
+                    (description.iloc[:, i]['75%'] - description.iloc[:, i]['25%']) * 2)
+        indices = list(data_set.iloc[:, i][
+                           (data_set.iloc[:, i] > limite_superior) | (data_set.iloc[:, i] < limite_inferior)].index)
+        indices_a_eliminar.extend(indices)
+
+indices_a_eliminar = set(sorted(indices_a_eliminar))
+# Teniendo 76 valores outliers, elimino esas filas
+len(indices_a_eliminar)
+
+data_set = data_set.drop(indices_a_eliminar)
+
+
+# Normalizo los datos que no son binarios
+def normalizacion_minmax(columna):
+    maximo = columna.max()
+    minimo = columna.min()
+    columnas_normalizadas = ((columna - minimo) / (maximo - minimo))
+    return columnas_normalizadas
+
+# Haciendo data_set.items() se forma un generator.
+for col_name, col in data_set.items():
+    if col.dtype == float:
+        data_set[col_name] = normalizacion_minmax(col)
+
+
+data_set[['age', 'imc', 'i_cin_cad', 'pre_art_sis', 'pre_art_dias', 'cvf_pre_por',
+          'vef1_cvf_pre_por', 'intima_m_c_d', 'intima_m_c_i']] = data_set[
+    ['age', 'imc', 'i_cin_cad', 'pre_art_sis', 'pre_art_dias', 'cvf_pre_por',
+     'vef1_cvf_pre_por', 'intima_m_c_d', 'intima_m_c_i']].apply(normalizacion_minmax, axis=0)
+
+for i in range(0, len(description.columns)):
+    print(description.iloc[:, i])
+
 # # Elimino filas segun valores outliers definidos
 # index_to_delete <- c()
 # index_to_delete <- c(which(data_set$intima_m_c_i > 5), which(data_set$hba1c > 500),
 #                      which(data_set$tri > 500), which(data_set$cldl < 0), which(data_set$cldl > 310),
 #                      which(data_set$vef1_cvf_pre_por > 300)) %>% unique() %>% sort(decreasing = TRUE)
 # data_set <- data_set[-c(index_to_delete), ]
-#
-#
+
+
 # #ANALISIS----
 # ## FORMULA ----
 # # Factores de riesgo = (ln(Edad) * 3.06117) + (ln(Colesterol total) * 1.12370) - (ln(Colesterol HDL) * 0.93263) + (ln(TA sistólica) * Factor TA sis) + Cig + DM - 23.9802
@@ -177,7 +196,8 @@ sns.violinplot(x = 'gender', y = 'dis_p', data = data_set)
 #
 # # Elimino las filas que tienen NA. A otra cosa jajaja
 # data_set <- na.omit(data_set)
-#
+
+
 # # Transformacion de los datos. Busco transformar el riesgo
 # Value <- data_set$riesgo
 # Group <- rep("riesgo", nrow(data_set))
