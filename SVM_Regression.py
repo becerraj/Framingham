ff0441f8d56d98130ab03660ba6dd202bb5f30f5
becerraj <becerra.jc99@gmail.com>
2023-09-09 19:40:21 -0300

    SVM Complete. Comments in spanish

--- SVM_Regression.py
+++ SVM_Regression.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
 import pdb
+import statsmodels.api as sm
 import openpyxl
 
-# #LOAD DATA----
+# LOAD DATA----
 # I load the data set
 data_set = pd.read_excel("/home/juan/Github/Framingham/normalized_data.xlsx")
 
@@ -17,8 +18,8 @@ train, test = train_test_split(data_set, test_size=0.3, train_size=0.7,
                                random_state=0)
 
 # Opcion 2: Pasando las variables y la columna predictora hace la separacion
-x = data_set.iloc[:, range(0, len(data_set.columns)-2)]
-y = data_set.iloc[:, len(data_set.columns)-1]
+x = data_set.iloc[:, range(0, len(data_set.columns) - 2)]
+y = data_set.iloc[:, len(data_set.columns) - 1]
 x_train, y_train, x_test, y_test = train_test_split(x, y,
                                                     test_size=0.3, train_size=0.7,
                                                     random_state=0)
@@ -28,7 +29,7 @@ x_train, y_train, x_test, y_test = train_test_split(x, y,
 # si los resultados son correctos, podria probarse para mostrarle al cliente.
 
 
-#3 Feature Scaling
+# 3 Feature Scaling
 # from sklearn.preprocessing import StandardScaler
 # sc_X = StandardScaler()
 # sc_y = StandardScaler()
@@ -47,19 +48,21 @@ prediction = model.predict(y_train)
 
 
 def bland_altman_plot(data1, data2, *args, **kwargs):
-    data1     = np.asarray(data1)
-    data2     = np.asarray(data2)
-    mean      = np.mean([data1, data2], axis=0)
-    diff      = data1 - data2                   # Difference between data1 and data2
-    md        = np.mean(diff)                   # Mean of the difference
-    sd        = np.std(diff, axis=0)            # Standard deviation of the difference
-    CI_low    = md - 1.96*sd
-    CI_high   = md + 1.96*sd
+    data1 = np.asarray(data1)
+    data2 = np.asarray(data2)
+    mean = np.mean([data1, data2], axis=0)
+    diff = data1 - data2  # Difference between data1 and data2
+    md = np.mean(diff)  # Mean of the difference
+    sd = np.std(diff, axis=0)  # Standard deviation of the difference
+    CI_low = md - 1.96 * sd
+    CI_high = md + 1.96 * sd
+    lowess = sm.nonparametric.lowess(diff, mean, frac=0.5)
 
     plt.scatter(mean, diff, *args, **kwargs)
-    plt.axhline(md,           color='black', linestyle='-')
-    plt.axhline(md + 1.96*sd, color='gray', linestyle='--')
-    plt.axhline(md - 1.96*sd, color='gray', linestyle='--')
+    plt.plot(lowess[:, 0], lowess[:, 1])
+    plt.axhline(md, color='black', linestyle='-')
+    plt.axhline(md + 1.96 * sd, color='gray', linestyle='--')
+    plt.axhline(md - 1.96 * sd, color='gray', linestyle='--')
     return md, sd, mean, CI_low, CI_high
 
 
@@ -67,24 +70,25 @@ md, sd, mean, CI_low, CI_high = bland_altman_plot(prediction, y_test)
 plt.title(r"$\mathbf{Bland-Altman}$" + " " + r"$\mathbf{SVM Plot}$")
 plt.xlabel("Risk")
 plt.ylabel("Error")
-plt.ylim(md - 3.5*sd, md + 3.5*sd)
+plt.ylim(md - 3.5 * sd, md + 3.5 * sd)
 
-BA_plot = np.min(mean) + (np.max(mean)-np.min(mean))*1.14
+BA_plot = np.min(mean) + (np.max(mean) - np.min(mean)) * 1.14
 
-plt.text(BA_plot, md - 1.96*sd,
-    r'-1.96SD:' + "\n" + "%.2f" % CI_low,
-    ha = "center",
-    va = "center",
-    )
-plt.text(BA_plot, md + 1.96*sd,
-    r'+1.96SD:' + "\n" + "%.2f" % CI_high,
-    ha = "center",
-    va = "center",
-    )
+plt.text(BA_plot, md - 1.96 * sd,
+         r'-1.96SD:' + "\n" + "%.2f" % CI_low,
+         ha="center",
+         va="center",
+         )
+plt.text(BA_plot, md + 1.96 * sd,
+         r'+1.96SD:' + "\n" + "%.2f" % CI_high,
+         ha="center",
+         va="center",
+         )
 plt.text(BA_plot, md,
-    r'Mean:' + "\n" + "%.2f" % md,
-    ha = "center",
-    va = "center",
-    )
+         r'Mean:' + "\n" + "%.2f" % md,
+         ha="center",
+         va="center",
+         )
 plt.subplots_adjust(right=0.85)
+
 plt.show()
